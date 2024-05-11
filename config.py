aa052a193cca281a8f40939f848a84150e401727
ymmmmmmmm <51306299+ymmmmmmmm@users.noreply.github.com>
2024-05-12 02:03:02 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+project_uuid = '91bf10e7-5806-460d-95af-bef2a3122e12'
+project_client_key = 'cOqbmrQ1YfOuBMo0KKDtd15bG1ENRoxuUa7nNO76'
+project_app_uuid = '79df412e-7e9d-4a19-8484-a2c8f3d65a2e'
+yescaptcha_client_key = '*****************************'
